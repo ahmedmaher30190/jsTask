92a4d2923750adcbbf5544774fd29ca69e56f533
Ahmed Maher <ahmedmaher@Ahmeds-MacBook-Air.local>
2021-07-10 17:24:19 +0200

    Heba Function's

--- index.js
+++ index.js
@@ -41,36 +41,58 @@ var lists = [
   },
 ];
 
+var output =[];
+var maxNoOfItems = 0;
+var maxNoOfItems2 = 0;
 
-var data = [];
-var number = 0;
-lists.forEach((item,x) => {
-  getNewData(x)
-});
-
-function getNewData(x){
-    data.push(lists[x]['sup']) ;
-    number += lists[x]['sup'].length;
-}
-
-
-var dat=[];
-function getLastData(){
-  for (var i = 0; i < 100; i++) {
-    if(data[i]){
-      for (var x = 0; x < 1; x++) {
-        dat.push(data[i][x])
-        data[i].splice(dat[x],1)
-      }
-    }
+function getemails (inputArray) {
+  var arrOfSups= inputArray.map(emails => emails.sup )
+  arrOfSups.forEach((item, i) => {
+    maxNoOfItems2+= item.length;
+  });
+  maxNoOfItems = arrOfSups.sort().reverse()[0].length
+  for (var i=0; i<maxNoOfItems ; i++){
+     for (var j=0; j<arrOfSups.length ; j++){
+       if(arrOfSups[j][i] !== undefined){
+           output.push(arrOfSups[j][i])
+       }
+     }
   }
+  console.log(maxNoOfItems, maxNoOfItems2);
 }
+getemails(lists);
 
-for (var i = 0; i < number; i++) {
-  getLastData()
-}
+
 //
-dat = dat.filter(function( element ) {
-   return element !== undefined;
-})
-console.log(dat)
+// var data = [];
+// var number = 0;
+// lists.forEach((item,x) => {
+//   getNewData(x)
+// });
+//
+// function getNewData(x){
+//     data.push(lists[x]['sup']) ;
+//     number += lists[x]['sup'].length;
+// }
+//
+//
+// var dat=[];
+// function getLastData(){
+//   for (var i = 0; i < 100; i++) {
+//     if(data[i]){
+//       for (var x = 0; x < 1; x++) {
+//         dat.push(data[i][x])
+//         data[i].splice(dat[x],1)
+//       }
+//     }
+//   }
+// }
+//
+// for (var i = 0; i < number; i++) {
+//   getLastData()
+// }
+// //
+// dat = dat.filter(function( element ) {
+//    return element !== undefined;
+// })
+// console.log(dat)
