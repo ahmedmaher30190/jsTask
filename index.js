904930eb816a4b0480faaf12ffa7aee2c9e58590
Ahmed Maher <ahmedmaher@Ahmeds-MacBook-Air.local>
2021-07-10 17:25:47 +0200

    Edit Objects value

--- index.js
+++ index.js
@@ -2,41 +2,40 @@ var lists = [
   {
     domain:'google.com',
     sup: [
-      '1@google.com',
-      '2@google.com',
-      '3@google.com',
-      '4@google.com',
-      '5@google.com',
-      '6@google.com',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
     ]
   },
   {
     domain:'facebook.com',
     sup: [
-      '1@facebook.com',
-      '2@facebook.com',
-      '3@facebook.com',
+      '1',
+      '2',
+      '3',
     ]
   },
   {
     domain:'whatsapp.com',
     sup: [
-      '1@whatsapp.com',
-      '2@whatsapp.com',
-      '3@whatsapp.com',
-      '4@whatsapp.com',
+      '1',
+      '2',
+      '3',
+      '4',
     ]
   },
   {
     domain:'sabelhost.com',
     sup: [
-      '1@sabelhost.com',
-      '2@sabelhost.com',
-      '3@sabelhost.com',
-      '4@sabelhost.com',
-      '5@sabelhost.com',
-      '6@sabelhost.com',
-      '7@sabelhost.com',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
     ]
   },
 ];
