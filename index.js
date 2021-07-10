dc4592dd3c97bae9c18de0bfa009a07069bdd6f3
Ahmed Maher <ahmedmaher@Ahmeds-MacBook-Air.local>
2021-07-10 03:04:33 +0200

    Not Profishnal resolve

--- index.js
+++ index.js
@@ -2,44 +2,72 @@ var lists = [
   {
     domain:'google.com',
     sup: [
-      'info@google.com',
-      'suport@google.com',
-      'carear@google.com',
-      'domain@google.com',
-      'director@google.com',
-      'sales@google.com',
+      '1@google.com',
+      '2@google.com',
+      '3@google.com',
+      '4@google.com',
+      '5@google.com',
+      '6@google.com',
     ]
   },
   {
     domain:'facebook.com',
     sup: [
-      'carear@facebook.com',
-      'domain@facebook.com',
-      'sales@facebook.com',
+      '1@facebook.com',
+      '2@facebook.com',
+      '3@facebook.com',
     ]
   },
   {
     domain:'whatsapp.com',
     sup: [
-      'info@whatsapp.com',
-      'suport@whatsapp.com',
-      'carear@whatsapp.com',
-      'domain@whatsapp.com',
+      '1@whatsapp.com',
+      '2@whatsapp.com',
+      '3@whatsapp.com',
+      '4@whatsapp.com',
     ]
   },
   {
     domain:'sabelhost.com',
     sup: [
-      'info@sabelhost.com',
-      'suport@sabelhost.com',
-      'carear@sabelhost.com',
-      'domain@sabelhost.com',
-      'director@sabelhost.com',
-      'sales@sabelhost.com',
+      '1@sabelhost.com',
+      '2@sabelhost.com',
+      '3@sabelhost.com',
+      '4@sabelhost.com',
+      '5@sabelhost.com',
+      '6@sabelhost.com',
+      '7@sabelhost.com',
     ]
   },
 ];
 
+var data = [];
+lists.forEach((item,x) => {
+  getNewData(x)
+});
+
+function getNewData(x){
+    data.push(lists[x]['sup']) ;
+}
 
 
-console.log(lists)
+var dat=[];
+function aaaa(){
+  for (var i = 0; i < 100; i++) {
+    if(data[i]){
+      for (var x = 0; x < 1; x++) {
+        dat.push(data[i][x])
+        data[i].splice(dat[x],1)
+      }
+    }
+  }
+}
+
+for (var i = 0; i < 100; i++) {
+  aaaa()
+}
+//
+dat = dat.filter(function( element ) {
+   return element !== undefined;
+})
+console.log(dat)
